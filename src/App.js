f5572f9e3972c879fb42fc5f06414753acb75d37
KihaJeong <jkha7371@gmail.com>
2021-07-30 17:44:09 +0900

    react start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { createRef, useMemo, useRef, useState } from 'react';
 
+// useRef(디자인)
+//dom을 변경할 때 사용
 function App() {
+  const myRef = useRef(null);
+  const [list, setList] = useState([
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '임꺽정' },
+  ]);
+  const myRefs = Array.from({ length: list.length }).map(() => createRef()); // 특정길이의 x값으로만 이루어진 배열 생성
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => {
+          console.log(myRef.current);
+          myRef.current.style.backgroundColor = 'red';
+          myRefs[0].current.style.backgroundColor = 'blue';
+        }}
+      >
+        색 변경
+      </button>
+      <div ref={myRef}>박스</div>
+      {list.map((user, index) => (
+        <h1 ref={myRefs[index]}>{user.name}</h1>
+      ))}
     </div>
   );
 }
---
+++ src/Sub.js
@@ -0,0 +1,12 @@
+// rcc 단축키로 한번에 생성 가능
+import React from 'react';
+
+const Sub = () => {
+  return (
+    <div>
+      <h1>Sub입니다.</h1>
+    </div>
+  );
+};
+
+export default Sub;
